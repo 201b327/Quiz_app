6443569051483a767bfa0bab1a259bc2a866e1e5
VidhiKuls <105033951+VidhiKuls@users.noreply.github.com>
2022-06-04 09:48:23 +0530

    question string

--- app/src/main/java/com/example/mentalhealthdetector/QuestionAnswer.java
+++ app/src/main/java/com/example/mentalhealthdetector/QuestionAnswer.java
@@ -1,3 +1,10 @@
 package com.example.mentalhealthdetector;
 
 public class QuestionAnswer {
+
+  public static String question[] ={
+            "Which company owns the android?",
+            "Which one is not the programming language?",
+            "Where you are watching this video?",
+            "Which company owns the Apple?"
+    };
